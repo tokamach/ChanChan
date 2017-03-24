0bb17c29c250cfd6f5a7381fcb51c109082c5a7d
Tom Hutchings <goerofmuns@gmail.com>
2017-03-24 22:26:35 +0000

    Initial Commit

---
+++ 4chan-client/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  4chan-client
+//
+//  Created by Tom Hutchings on 24/03/2017.
+//  Copyright © 2017 tokamach. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 4chan-client/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  4chan-client
+//
+//  Created by Tom Hutchings on 24/03/2017.
+//  Copyright © 2017 tokamach. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
